b885157f18a922d12f707f593c7654ca39d4cf1b
juanka2023 <bull-ter@hotmail.es>
2023-04-17 11:06:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/musica1/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.musica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button play_pause, boton_repetir;
+    MediaPlayer mp;
+    ImageView iv;
+    int repetir = 2, posicion = 0;
+
+
+    MediaPlayer vectormp [] = new MediaPlayer[3];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play_pause = (Button) findViewById(R.id.botonplay);
+        boton_repetir = (Button) findViewById(R.id.btnrepetir);
+        iv = (ImageView) findViewById(R.id.imageView);
+
+        vectormp[0] = MediaPlayer.create(this, R.raw.race);
+        vectormp[1] = MediaPlayer.create(this, R.raw.sound);
+        vectormp[2] = MediaPlayer.create(this, R.raw.tea);
+
+    }
+    //metodo play y pause
+    public void PlayPause (View view){
+
+        if(vectormp[posicion].isPlaying()){
+            vectormp[posicion].pause();
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
+        }else{
+            vectormp[posicion].start();
+            play_pause.setBackgroundResource(R.drawable.pausa);
+            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+        }
+    }
+    // metodo del boton stop
+    public void Stop(View view){
+        if(vectormp[posicion] != null){
+            vectormp[posicion].stop();
+
+            vectormp[0] = MediaPlayer.create(this, R.raw.race);
+            vectormp[1] = MediaPlayer.create(this, R.raw.sound);
+            vectormp[2] = MediaPlayer.create(this, R.raw.tea);
+            posicion = 0;
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            iv.setImageResource(R.drawable.portada1);
+            Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    //Método repetir pista
+    public void Repetir(View view){
+        if(repetir == 1){
+            boton_repetir.setBackgroundResource(R.drawable.repetir);
+            Toast.makeText(this, "No repetir", Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(false);
+            repetir = 2;
+        } else {
+            boton_repetir.setBackgroundResource(R.drawable.repetir);
+            Toast.makeText(this, "Repetir", Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(true);
+            repetir = 1;
+        }
+    }
+}
+
+
+
